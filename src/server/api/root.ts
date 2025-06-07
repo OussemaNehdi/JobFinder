c82f5dcb2fb4ac1fceb9b947c6bd9b279184b95b
OussemaNehdi <oussema.nehdi2@gmail.com>
2025-06-07 12:43:30 +0200

    fixing bug v3

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,8 +1,17 @@
 import { jobRouter } from "./routers/job";
-import { createTRPCRouter } from "./trpc";
+import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 export const appRouter = createTRPCRouter({
   job: jobRouter,
 });
 
 export type AppRouter = typeof appRouter
+
+/**
+ * Create a server-side caller for the tRPC API.
+ * @example
+ * const trpc = createCaller(createContext);
+ * const res = await trpc.post.all();
+ *       ^? Post[]
+ */
+export const createCaller = createCallerFactory(appRouter);
