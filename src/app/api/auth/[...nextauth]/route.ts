fd89f9da2c01847ca10937282629c5b0835f7f14
OussemaNehdi <oussema.nehdi2@gmail.com>
2025-08-01 23:08:23 +0200

    hotfix deployment

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,7 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
-
-const prisma = new PrismaClient(); // For clear presentation, however I could import db from server/db if needed
+import { db } from "~/server/db";
 
 
 export const authOptions: NextAuthOptions = {
@@ -17,7 +15,7 @@ export const authOptions: NextAuthOptions = {
       },
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) return null;
-        const user = await prisma.user.findUnique({ where: { email: credentials.email } });
+        const user = await db.user.findUnique({ where: { email: credentials.email } });
         if (!user) return null;
         const valid = await bcrypt.compare(credentials.password, user.password);
         if (!valid) return null;
